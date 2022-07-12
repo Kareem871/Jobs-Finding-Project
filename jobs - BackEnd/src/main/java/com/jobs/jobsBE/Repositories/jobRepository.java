d313a2bacf7650a9bef7de72b11285cbc4b11267
Kareem871 <62218150+Kareem871@users.noreply.github.com>
2022-07-12 12:23:14 +0300

    Delete jobs - BackEnd directory

--- jobs - BackEnd/src/main/java/com/jobs/jobsBE/Repositories/jobRepository.java
+++
@@ -1,8 +0,0 @@
-package com.jobs.jobsBE.Repositories;
-
-import com.jobs.jobsBE.models.job;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface jobRepository extends MongoRepository<job, String> {
-
-}
